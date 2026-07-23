d82ecd9cfc8fda8e28d80f31a143c91fd59b80de
ColinDev <colinpham@proton.me>
2026-07-23 18:49:17 +0200

    Added level map and loader + tilemap

---
+++ src/map.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cpl/cpl.h>
+
+#define TILE_SIZE 50
+
+enum {
+    LEVEL_0 = 0,
+    TOTAL_LEVELS
+};
+
+void init_levels();
+tilemap *get_level_tilemap(int level);
+void draw_level(int level);
